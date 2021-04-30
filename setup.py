fb148d7937b280a60c19b445de27b6b3f8da2013
Eddie Cho <eunseocho@gmail.com>
2021-04-30 14:42:54 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+import io
+import os
+
+CURRENT_DIR = os.path.dirname(__file__)
 
 test_deps=[
     'pytest',
@@ -25,7 +29,7 @@ setup(
     url='https://github.com/neo-ai/neo-ai-dlr',
     license = "Apache-2.0",
     description = 'Common libraries for converting machine learning models to TVM Relay IR',
-    long_description=io.open(os.path.join(CURRENT_DIR, '../README.md'), encoding='utf-8').read(),
+    long_description=io.open(os.path.join(CURRENT_DIR, 'README.md'), encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
